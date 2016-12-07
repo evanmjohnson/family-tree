5eedc428708e522228fd3cbe7678f7193ac191c5
EVAN <johnson.ev@husky.neu.edu>
2016-12-06 22:37:29 -0500

    Implemented inserting a person and address

--- src/Insert.java
+++ src/Insert.java
@@ -23,7 +23,7 @@ public class Insert {
    * menu. Delegates all insert operations to helper methods.
    */
   public void insert() {
-    System.out.println("What do you want to insert? A person, address, house, reunion," +
+    System.out.print("What do you want to insert? A person, address, house, reunion," +
             "or relationship?");
     Scanner scan = new Scanner(System.in);
     String toUpdate = scan.nextLine();
@@ -46,40 +46,98 @@ public class Insert {
     }
   }
 
+  /**
+   * Inserts a person to the database with the prompted fields.
+   */
   private void insertPerson() {
-    System.out.println("Enter the first name of the new person: ");
+    System.out.print("Enter the first name of the new person: ");
     Scanner scanner = new Scanner(System.in);
     String newFirstName = scanner.next();
-    System.out.println("Enter the last name of the new person: ");
+    System.out.print("Enter the last name of the new person: ");
     String newLast = scanner.next();
-    System.out.println("Enter the DOB of the new person (YYYY-MM-DD): ");
+    System.out.print("Enter the year of birth of the new person: ");
     String newDOB = scanner.next();
-    System.out.println("Enter the DOD of the new person (YYYY-MM-DD): ");
-    String newDOD = scanner.next();
+    System.out.print("Is this person alive? Y/N ");
+    String yesNo = scanner.next();
+    String newDOD = null;
+    if (yesNo.equalsIgnoreCase("N")) {
+      System.out.print("What is the year of the new person's death? ");
+      newDOD = scanner.next();
+    }
     try {
       Statement statement = connection.createStatement();
-      ResultSet resultSet1 = statement.executeQuery("SELECT MAX person_id FROM person");
+      ResultSet resultSet1 = statement.executeQuery("SELECT MAX(person_id) FROM person");
+      resultSet1.first();
       int id = Integer.parseInt(resultSet1.getString(1));
-      ResultSet resultSet = statement.executeQuery("INSERT INTO person VALUES (" + "\'" + newLast + "\'"
-      + "\'" + newFirstName + "\'" + (id + 1)  +  "\'"  + newDOB + "\'" +  "\'" + newDOD +  "\'" );
+      if (newDOD == null) {
+        statement.executeUpdate("INSERT INTO person VALUES (" + "\'" + newFirstName + "\',"
+            + "\'" + newLast + "\'," + newDOB + ",NULL," + (id + 1) + ")");
+      }
+      else {
+        statement.executeUpdate("INSERT INTO person VALUES (" + "\'" + newFirstName + "\',"
+            + "\'" + newLast + "\'," + newDOB + "," + newDOD + "," + (id + 1) + ")");
+      }
     } catch (SQLException e) {
       e.printStackTrace();
     }
 
   }
 
+  /**
+   * Inserts an address to the database with the prompted fields.
+   */
   private void insertAddress() {
-
+    System.out.print("Enter the street of the new address: ");
+    Scanner scan = new Scanner(System.in);
+    scan.useDelimiter("\\n");
+    String street = scan.next();
+    System.out.print("Enter the city of the new address: ");
+    String city = scan.next();
+    System.out.print("Does this address have a state? Y/N");
+    String yesNo = scan.next();
+    String state = null;
+    if (yesNo.equalsIgnoreCase("Y")) {
+      System.out.print("What is the state? ");
+      state = scan.next();
+    }
+    System.out.print("What is the country of the new address? ");
+    String country = scan.next();
+    try {
+      Statement statement = connection.createStatement();
+      ResultSet resultSet1 = statement.executeQuery("SELECT MAX(house_id) FROM address");
+      resultSet1.first();
+      int id = Integer.parseInt(resultSet1.getString(1));
+      if (state == null) {
+        statement.executeUpdate("INSERT INTO address VALUES (\'" + street +
+            "\',\'" + city + "\'," + "NULL" + ",\'" + country + "\'," + (id + 1) + ")");
+      }
+      else {
+        statement.executeUpdate("INSERT INTO address VALUES (\'" + street +
+            "\',\'" + city + "\',\'" + state + "\',\'" + country + "\'," + (id + 1) + ")");
+      }
+    }
+    catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
+  /**
+   * Inserts a house to the database with the prompted fields.
+   */
   private void insertHouse() {
 
   }
 
+  /**
+   * Inserts a reunion to the database with the prompted fields.
+   */
   private void insertReunion() {
 
   }
 
+  /**
+   * Inserts a relationship to the database with the prompted fields.
+   */
   private void insertRelationship() {
 
   }
