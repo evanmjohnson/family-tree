086001825249723c55facb7391abbbf1eb5965ab
EVAN <johnson.ev@husky.neu.edu>
2016-12-06 23:51:04 -0500

    Finished and tested all inserts

--- src/Insert.java
+++ src/Insert.java
@@ -24,7 +24,7 @@ public class Insert {
    */
   public void insert() {
     System.out.print("What do you want to insert? A person, address, house, reunion," +
-            "or relationship?");
+            "or relationship? ");
     Scanner scan = new Scanner(System.in);
     String toUpdate = scan.nextLine();
     if (toUpdate.equalsIgnoreCase("person")) {
@@ -125,20 +125,83 @@ public class Insert {
    * Inserts a house to the database with the prompted fields.
    */
   private void insertHouse() {
-
+    System.out.print("Enter the first name of the person that lives in this house: ");
+    Scanner scan = new Scanner(System.in);
+    String firstName = scan.next();
+    int personID = Utils.selectPersonFromFirstName(firstName, connection);
+    System.out.print("How many people live in this house? ");
+    int numResidents = scan.nextInt();
+    System.out.print("Enter the first name of the head of this house: ");
+    String headFirst = scan.next();
+    int headID = Utils.selectPersonFromFirstName(headFirst, connection);
+    try {
+      Statement statement = connection.createStatement();
+      ResultSet resultSet = statement.executeQuery("SELECT MAX(house_id) FROM house");
+      resultSet.first();
+      int houseID = Integer.parseInt(resultSet.getString(1));
+      statement.executeUpdate("INSERT INTO house VALUES (" + personID + "," + (houseID + 1) +
+          "," + numResidents + "," + headID + ")");
+    }
+    catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
   /**
    * Inserts a reunion to the database with the prompted fields.
    */
   private void insertReunion() {
-
+    System.out.print("Enter the first name of the head of the house that is hosting this reunion: ");
+    Scanner scan = new Scanner(System.in);
+    String headFirst = scan.next();
+    int headID = Utils.selectPersonFromFirstName(headFirst, connection);
+    System.out.print("How many families are attending this reunion? ");
+    int numFamilies = scan.nextInt();
+    System.out.print("What street is the reunion hosted on? ");
+    scan.useDelimiter("\\n");
+    String street = scan.next();
+    int houseID = Utils.getHouseIDFromStreet(street, connection);
+    System.out.print("What is the occasion of this reunion? ");
+    String occasion = scan.next();
+    System.out.print("What is the date (YYYY-MM-DD) of this reunion? ");
+    String date = scan.next();
+    try {
+      Statement statement = connection.createStatement();
+      ResultSet resultSet = statement.executeQuery("SELECT MAX(reunion_id) FROM Reunion");
+      resultSet.first();
+      int reunionID = Integer.parseInt(resultSet.getString(1));
+      String query = "INSERT INTO Reunion VALUES (" + (reunionID + 1) + "," + headID +
+          "," + numFamilies + "," + houseID + ",\'" + occasion + "\',\'" + date + "\')";
+      statement.executeUpdate(query);
+    }
+    catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 
   /**
    * Inserts a relationship to the database with the prompted fields.
    */
   private void insertRelationship() {
-
+    System.out.print("Enter the first name of the first person in the relationship: ");
+    Scanner scan = new Scanner(System.in);
+    String firstFirst = scan.next();
+    int first = Utils.selectPersonFromFirstName(firstFirst, connection);
+    System.out.print("Enter the first name of the sceond person in the relationship: ");
+    String firstSecond = scan.next();
+    int second = Utils.selectPersonFromFirstName(firstSecond, connection);
+    System.out.print("What is the relationship between these two people? ");
+    String relationship = scan.next();
+    try {
+      Statement statement = connection.createStatement();
+      ResultSet resultSet = statement.executeQuery("SELECT MAX(relation_id) FROM relationship");
+      resultSet.first();
+      int relationID = Integer.parseInt(resultSet.getString(1));
+      statement.executeUpdate("INSERT INTO relationship VALUES (" + first + "," + second + "," +
+          "\'" + relationship + "\'," + (relationID + 1) + ")");
+    }
+    catch (SQLException e) {
+      e.printStackTrace();
+    }
   }
 }
